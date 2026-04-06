1695606dbb27383127f7a9de791397f89024e14e
justtryhard <lexchernobrov@gmail.com>
2026-04-06 11:34:28 +0300

    covering tests (3)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,12 @@
 from src.api.main import app
 from fastapi.testclient import TestClient
 from unittest.mock import patch
-from src.api.auth import create_token
+from src.api.auth import create_token, verify_token
 from src.models.product import Product
 from src.models.user import User
+from jose import jwt, JWTError
+import pytest
+
 
 client = TestClient(app)
 
@@ -82,6 +85,26 @@ def test_create_order_product_not_found(mock_products, mock_users):
     })
     assert response.status_code == 400
 
+
+@patch("src.data.users")
+@patch("src.data.products")
+def test_create_order_one_of_products_not_found(mock_products, mock_users):
+    mock_users.return_value = User(name="test", email="a@b.com")
+    mock_products.return_value = {1: Product(name="bread", price=2.5, quantity=1)}
+    response = client.post("/api/v1/orders/", json={
+        "user_id": 1,
+        "product_ids": [1, 459]
+    })
+    assert response.status_code == 400
+
+def test_create_order_without_products():
+    response = client.post("/api/v1/orders/", json={
+        "user_id": 1,
+        "product_ids": []
+    })
+    assert response.status_code == 422
+
+
 def test_create_order_invalid_user():
     response = client.post("/api/v1/orders/", json={
         "user_id": "abc",
@@ -96,6 +119,21 @@ def test_create_order_invalid_product():
     })
     assert response.status_code == 422
 
+def test_create_order_without_products_field():
+    response = client.post("/api/v1/orders/", json={
+        "user_id": 1,
+    })
+    assert response.status_code == 422
+
+def test_create_order_without_user_field():
+    response = client.post("/api/v1/orders/", json={
+        "product_ids": [1, 2, 3]
+    })
+    assert response.status_code == 422
+
+def test_create_order_without_field():
+    response = client.post("/api/v1/orders/", json={})
+    assert response.status_code == 422
 
 
 def test_create_user():
@@ -124,10 +162,90 @@ def test_create_user_without_email():
     })
     assert response.status_code == 422
 
+def test_create_user_without_fields():
+    response = client.post("/api/v1/users", json={})
+    assert response.status_code == 422
+
 def test_get_user():
     response = client.get("/api/v1/users/1")
     assert response.status_code == 200
 
+def test_get_user_invalid():
+    response = client.get("/api/v1/users/asd")
+    assert response.status_code == 422
+
 def test_user_not_found():
     response = client.get("/api/v1/users/345345436")
     assert response.status_code == 404
+
+
+@patch("src.api.routes.exchange.exchange_client")
+def test_convert_price(mock_client):
+    mock_client.get_exchange_rate.return_value = 2.0
+    response = client.get("api/v1/currency/convert", params={
+        "price": 100,
+        "from_currency": "USD",
+        "to_currency": "EUR"
+    })
+    assert response.status_code == 200
+    assert response.json() == 200
+    mock_client.get_exchange_rate.assert_called_once_with("USD", "EUR")
+
+
+def test_convert_same_currency():
+    response = client.get("api/v1/currency/convert", params={
+        "price": 50,
+        "from_currency": "USD",
+        "to_currency": "USD"
+    })
+    assert response.status_code == 200
+    assert response.json() == 50
+
+
+@patch("src.api.routes.exchange.exchange_client")
+def test_convert_service_unavailable(mock_client):
+    mock_client.get_exchange_rate.return_value = None
+    response = client.get("api/v1/currency/convert", params={
+        "price": 100,
+        "from_currency": "USD",
+        "to_currency": "EUR"
+    })
+    assert response.status_code == 503
+    data = response.json()
+    assert data["detail"] == "service unavailable"
+
+
+@patch("src.api.routes.exchange.exchange_client")
+def test_convert_zero_price(mock_client):
+    mock_client.get_exchange_rate.return_value = 2.0
+    response = client.get("api/v1/currency/convert", params={
+        "price": 0,
+        "from_currency": "USD",
+        "to_currency": "EUR"
+    })
+    assert response.status_code == 200
+    assert response.json() == 0
+
+
+@patch("src.api.routes.exchange.exchange_client")
+def test_convert_invalid_currency(mock_client):
+    mock_client.get_exchange_rate.return_value = None
+    response = client.get("api/v1/currency/convert", params={
+        "price": 100,
+        "from_currency": "dsdsdf",
+        "to_currency": "wegdre"
+    })
+    assert response.status_code == 503
+    assert response.json()["detail"] == "service unavailable"
+
+
+def test_create_token_returns_str():
+    token = create_token(user_id=123)
+    assert isinstance(token, str)
+
+def test_verify_token_valid():
+    token = create_token(user_id=456)
+    user_id = verify_token(token)
+    assert user_id == 456
+
+
